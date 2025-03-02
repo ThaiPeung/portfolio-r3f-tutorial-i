41a928ac8f1cb68bd8db1b1a7919771d6dc0401c
qlCamperlp <miwot1234@gmail.com>
2025-03-02 11:38:11 +0700

    add project page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import HomePage from "./home/page";
 
 export default function Home() {
   return (
-    <main className="bg-slate-300/20">
+    <main className="bg-slate-300/20 h-[100vh]">
       <HomePage />
     </main>
   );
